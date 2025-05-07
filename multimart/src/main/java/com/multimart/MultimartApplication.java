64fe1ff7171b00ded48e14793e09dc9307d872de
Nikkk28 <marthanikshith.28@gmail.com>
2025-05-07 17:56:10 +0530

    initial commit-(start.spring.io)

---
+++ multimart/src/main/java/com/multimart/MultimartApplication.java
@@ -0,0 +1,13 @@
+package com.multimart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimartApplication.class, args);
+	}
+
+}
