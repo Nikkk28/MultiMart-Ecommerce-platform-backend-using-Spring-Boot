1c46005a7950df2e6411d7470ce7a9dc583b9d65
Nikkk28 <marthanikshith.28@gmail.com>
2025-05-07 18:10:56 +0530

    dto auth

---
+++ multimart/src/main/java/com/multimart/dto/auth/LoginRequest.java
@@ -0,0 +1,21 @@
+package com.multimart.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    
+    private String username;
+    
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    private String password;
+}
